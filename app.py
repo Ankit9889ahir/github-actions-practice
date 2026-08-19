aa5fa622fffb51b5eb612f49fb87d77b4fd078b5
Ankit9889ahir <91006669+Ankit9889ahir@users.noreply.github.com>
2026-08-19 17:00:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-#this code is for small flask app
 from flask import Flask, render_template
 app = Flask(__name__)
 
-#jobs
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
 
 @app.route('/health')
 def health():
