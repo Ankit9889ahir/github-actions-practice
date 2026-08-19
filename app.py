92549cb4cf66ca5951c609ecac9faac6d61e04b9
Ankit9889ahir <91006669+Ankit9889ahir@users.noreply.github.com>
2026-08-19 16:33:25 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+#this code is for small flask app
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/health')
+def health():
+    return 'Server is up and running'
